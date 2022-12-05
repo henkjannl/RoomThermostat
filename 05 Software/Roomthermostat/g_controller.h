08767d297235b0c0a6a41b42962e806b41d0462d
henkjannl <henkjanvanderpol@gmail.com>
2022-12-05 19:24:59 +0100

    Rename template.json

--- 05 Software/Roomthermostat/g_controller.h
+++ 05 Software/Roomthermostat/g_controller.h
@@ -1,90 +1,5 @@
 #pragma once
 
-#include <OpenTherm.h>
-
-#define CONTROLLER_INTERVAL 5000
-
-/* ======================================
-   PID CONTROLLER CLASS DEFINITION
-   ====================================== */
-class PID {
-  public:
-    float KP = 10;
-    float KI = 0.02;  
-    float KD = 0;
-
-    // Upper and lower bounds on requested boiler temperature
-    float outputHigh = 80;
-    float outputLow  =  0;
-      
-    float setpoint=4.0;
-    
-    unsigned long currentTime;
-    unsigned long previousTime;
-    
-    float currentTemperature;
-    float previousTemperature;
-
-    float boilerTemperature;
-    float boilerPercentage;
-
-    float timeStep;
-
-    float ierr = 0;
-    float previousError = 0;
-    float derivative = 0;
-
-    float P;
-    float I;
-    float D;
-    
-    float newValue(float newTemperature);
-};
-
-float PID::newValue(float newTemperature) {
-
-  // Update temperature
-  previousTemperature = currentTemperature;
-  currentTemperature  = newTemperature;
-
-  // Store time in native format to ensure right outcome on millis() overflow 
-  previousTime=currentTime;
-  currentTime=millis();
-  timeStep=(currentTime-previousTime)/1000; // converted to seconds here
-    
-  // Calculate the control error
-  float error = setpoint - currentTemperature;
-  
-  // Calculate the integral error
-  ierr += error * timeStep;  
-  
-  // !! D-action probably not implemented well yet !!
-  // perhaps we need to filter the signal to prevent noise in the D-action
-  // Calculate the measurement derivative
-  derivative = (timeStep!=0) ? (error - previousError) / timeStep : 0;
-  
-  // Calculate the PID output
-  P = KP * error;        // proportional contribution
-  I = KI * ierr;         // integral contribution  
-  D = KD * derivative;   // differential contribution
-  boilerTemperature = P+I+D;
-  
-  // Implement anti-windup
-  // !! Should the (error<0) clause be there? !! What does it do?
-  if ((boilerTemperature < outputLow)  and (error<0)) ierr -= error * timeStep;    
-  if ((boilerTemperature > outputHigh) and (error>0)) ierr -= error * timeStep;    
-  
-  // Clip output
-  boilerTemperature = max(outputLow, min(outputHigh, boilerTemperature));
-  boilerPercentage=(boilerTemperature-outputLow)/(outputHigh-outputLow);
-  
-  //Serial.printf("setpoint= %.3f°C measured=%.3f°C P=%.3f I=%.3f D=%.3f boiler=%.3f°C\n",setpoint, currentTemperature, P, I, D, boilerTemperature);
-
-  previousError = error;
-
-  return boilerTemperature;
-};
-
 /* =================================================
  *  Controller program class definition
  * ================================================= */
@@ -96,7 +11,6 @@ class Controller {
 
   protected:
     ControllerData_t  *controllerData;
-    PID pidController;    
 
     // Various handlers for the user interface, both device and Telegram
     void control();
@@ -311,31 +225,6 @@ void Controller::control() {
     controllerData->overrideSetpoint = int(controllerData->temperatureSetpoint+0.5);
   }
 
-  pidController.setpoint=controllerData->temperatureSetpoint;
-
-  // Ask PID controller for new value
-  controllerData->requestedBoilerTemperature = pidController.newValue( controllerData->measuredTemperature() );
-  controllerData->boilerPercentage=pidController.boilerPercentage;
-
-  // Switch off central heating if it becomes too hot in the room
-  controllerData->enableCentralHeating = (controllerData->measuredTemperature() < (controllerData->temperatureSetpoint+2.0));
-
-  // Also switch off heater if we request almost no heat
-  if(controllerData->boilerPercentage < 0.05) controllerData->enableCentralHeating=false;
-
-  if (controllerData->flameOn) {
-    if      (controllerData->boilerPercentage < 0.05) controllerData->dspFlameIcon = iconFlame0; 
-    else if (controllerData->boilerPercentage < 0.10) controllerData->dspFlameIcon = iconFlame1; 
-    else if (controllerData->boilerPercentage < 0.15) controllerData->dspFlameIcon = iconFlame2; 
-    else if (controllerData->boilerPercentage < 0.20) controllerData->dspFlameIcon = iconFlame3; 
-    else if (controllerData->boilerPercentage < 0.35) controllerData->dspFlameIcon = iconFlame4; 
-    else if (controllerData->boilerPercentage < 0.50) controllerData->dspFlameIcon = iconFlame5; 
-    else if (controllerData->boilerPercentage < 0.65) controllerData->dspFlameIcon = iconFlame6; 
-    else if (controllerData->boilerPercentage < 0.80) controllerData->dspFlameIcon = iconFlame7; 
-    else                                              controllerData->dspFlameIcon = iconFlame8; 
-  }
-  else controllerData->dspFlameIcon = iconFlame0;
-
   for(int i=0; i<7; i++) {
     daytype=controllerData->regularWeek[dayOfWeek];  // 0=Sunday
     controllerData->dayIcons[i]=DAYTYPE_TO_ICON[daytype];
@@ -359,14 +248,6 @@ void Controller::control() {
   if(controllerData->overrideTomorrow!=dtAuto) {
     controllerData->dayIcons[1]=DAYTYPE_OVERRULED_TO_ICON[controllerData->overrideTomorrow];
   }
-
-  controllerData->P          = pidController.P;
-  controllerData->I          = pidController.I;
-  controllerData->D          = pidController.D;
-  controllerData->ierr       = pidController.ierr;
-  controllerData->derivative = pidController.derivative;
-
-  //controllerData->logBusyTime.finish(btController);  
 
   // Log the boiler actions at the whole hour
   if( (currTime.minutesSinceMidnight % 60 == 0) and (prevTime.minutesSinceMidnight % 60 == 59) ) {
--- 05 Software/Roomthermostat/h_temperature.h
+++ 05 Software/Roomthermostat/h_temperature.h
@@ -6,16 +6,98 @@
 
 #define RECONNECT_INTERVAL   5*60*1000
 #define TEMP_MEAS_INTERVAL     30*1000 
-#define BOILER_INTERVAL      3*60*1000
+#define BOILER_INTERVAL        60*1000
 
 OneWire oneWire(PIN_ONE_WIRE_BUS);                         // Bus for the temperature sensors 
 DallasTemperature dallasSensor(&oneWire);                  // Driver for temperature sensors
 OpenTherm opentherm(PIN_OPENTHERM_IN, PIN_OPENTHERM_OUT);  // Driver for boiler
+PID pidController;    
 
 //Interrupthandler for OpenTherm
 void ICACHE_RAM_ATTR handleInterrupt() {
   opentherm.handleInterrupt();
 }
+
+/* ======================================
+   PID CONTROLLER CLASS DEFINITION
+   ====================================== */
+class PID {
+  public:
+    float KP = 10;
+    float KI = 0.02;  
+    float KD = 0;
+
+    // Upper and lower bounds on requested boiler temperature
+    float outputHigh = 80;
+    float outputLow  =  0;
+      
+    float setpoint=4.0;
+    
+    unsigned long currentTime;
+    unsigned long previousTime;
+    
+    float currentTemperature;
+    float previousTemperature;
+
+    float boilerTemperature;
+    float boilerPercentage;
+
+    float timeStep;
+
+    float ierr = 0;
+    float previousError = 0;
+    float derivative = 0;
+
+    float P;
+    float I;
+    float D;
+    
+    float newValue(float newTemperature);
+};
+
+float PID::newValue(float newTemperature) {
+
+  // Update temperature
+  previousTemperature = currentTemperature;
+  currentTemperature  = newTemperature;
+
+  // Store time in native format to ensure right outcome on millis() overflow 
+  previousTime=currentTime;
+  currentTime=millis();
+  timeStep=(currentTime-previousTime)/1000; // converted to seconds here
+    
+  // Calculate the control error
+  float error = setpoint - currentTemperature;
+  
+  // Calculate the integral error
+  ierr += error * timeStep;  
+  
+  // !! D-action probably not implemented well yet !!
+  // perhaps we need to filter the signal to prevent noise in the D-action
+  // Calculate the measurement derivative
+  derivative = (timeStep!=0) ? (error - previousError) / timeStep : 0;
+  
+  // Calculate the PID output
+  P = KP * error;        // proportional contribution
+  I = KI * ierr;         // integral contribution  
+  D = KD * derivative;   // differential contribution
+  boilerTemperature = P+I+D;
+  
+  // Implement anti-windup
+  // !! Should the (error<0) clause be there? !! What does it do?
+  if ((boilerTemperature < outputLow)  and (error<0)) ierr -= error * timeStep;    
+  if ((boilerTemperature > outputHigh) and (error>0)) ierr -= error * timeStep;    
+  
+  // Clip output
+  boilerTemperature = max(outputLow, min(outputHigh, boilerTemperature));
+  boilerPercentage=(boilerTemperature-outputLow)/(outputHigh-outputLow);
+  
+  //Serial.printf("setpoint= %.3f°C measured=%.3f°C P=%.3f I=%.3f D=%.3f boiler=%.3f°C\n",setpoint, currentTemperature, P, I, D, boilerTemperature);
+
+  previousError = error;
+
+  return boilerTemperature;
+};
 
 
 void TemperatureMeasure();
@@ -100,6 +182,37 @@ void BoilerCommunicate() {
   unsigned long response;
   
   Serial.println("BoilerCommunicate()");
+
+  pidController.setpoint=controllerData.temperatureSetpoint;
+
+  // Ask PID controller for new value
+  controllerData.requestedBoilerTemperature = pidController.newValue( controllerData.measuredTemperature() );
+  controllerData.boilerPercentage=pidController.boilerPercentage;
+  
+  controllerData.P          = pidController.P;
+  controllerData.I          = pidController.I;
+  controllerData.D          = pidController.D;
+  controllerData.ierr       = pidController.ierr;
+  controllerData.derivative = pidController.derivative;
+
+  // Switch off central heating if it becomes too hot in the room
+  controllerData.enableCentralHeating = (controllerData.measuredTemperature() < (controllerData.temperatureSetpoint+2.0));
+
+  // Also switch off heater if we request almost no heat
+  if(controllerData.boilerPercentage < 0.05) controllerData.enableCentralHeating=false;
+
+  if (controllerData.flameOn) {
+    if      (controllerData.boilerPercentage < 0.05) controllerData.dspFlameIcon = iconFlame0; 
+    else if (controllerData.boilerPercentage < 0.10) controllerData.dspFlameIcon = iconFlame1; 
+    else if (controllerData.boilerPercentage < 0.15) controllerData.dspFlameIcon = iconFlame2; 
+    else if (controllerData.boilerPercentage < 0.20) controllerData.dspFlameIcon = iconFlame3; 
+    else if (controllerData.boilerPercentage < 0.35) controllerData.dspFlameIcon = iconFlame4; 
+    else if (controllerData.boilerPercentage < 0.50) controllerData.dspFlameIcon = iconFlame5; 
+    else if (controllerData.boilerPercentage < 0.65) controllerData.dspFlameIcon = iconFlame6; 
+    else if (controllerData.boilerPercentage < 0.80) controllerData.dspFlameIcon = iconFlame7; 
+    else                                              controllerData.dspFlameIcon = iconFlame8; 
+  }
+  else controllerData.dspFlameIcon = iconFlame0;
 
   // Current day and time
   time_t now;
